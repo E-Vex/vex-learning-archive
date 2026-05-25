6d3ff6e52aaee233b77a04ce208bcf7e18d23eb3
elia <raiedaleve@gmail.com>
2026-05-26 02:28:58 +0300

    Struct into Struct & typedef

---
+++ Files/Day9_StructIntoStruct&Typedef.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct
+{
+    unsigned int src_ip;
+    unsigned int dst_ip;
+} IP_header;
+
+typedef struct
+{
+    unsigned short src_port;
+    unsigned short dst_port;
+} TCP_header;
+
+typedef struct Packet
+{
+    char data[1024];
+    IP_header ip;
+    TCP_header tcp;
+
+} Packet;
+
+int main()
+{
+
+    return 0;
+}
