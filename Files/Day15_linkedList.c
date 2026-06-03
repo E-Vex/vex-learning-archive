cd2b83fff3e2dc5ca5aaff965f4faa6c72ef5cf0
elia <raiedaleve@gmail.com>
2026-06-04 01:09:08 +0300

    add file

--- Files/Day15_linkedList.c
+++ Files/Day15_linkedList.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+typedef struct int_Node
 {
     int data;
-    struct Node *next;
+    struct int_Node *next;
 
-} Node;
+} int_Node;
 
-Node *createNode(int data)
+int_Node *node_data(int data)
 {
 
-    Node *node = malloc(sizeof(Node));
+    int_Node *node = malloc(sizeof(int_Node));
     node->data = data;
     node->next = NULL;
 
@@ -20,8 +20,32 @@ Node *createNode(int data)
 
 int main()
 {
-    Node *head = createNode(10);
-    printf("Node data: %d\n", head->data);
+    int_Node *head = node_data(10); // Create head node
+    // head->data = 10;
+    // head->next = NULL;
+
+    int_Node *second = node_data(20); // Create second node
+    // second->data = 20;
+    // second->next = NULL;
+
+    head->next = second; // Link head to second
+
+    int_Node *third = node_data(30); // Create third nodes
+    // third->data = 30;
+    // third->next = NULL;
+
+    second->next = third; // Link second to third
+
+    /*---------------------------------------------------------------------------------------*/
+
+    int_Node *currentPointer = head;
+    int i = 1;
+    while (currentPointer != NULL)
+    {
+        printf("Node %d : %d\n", i, currentPointer->data);
+        currentPointer = currentPointer->next;
+        i++;
+    }
 
     return 0;
 }
