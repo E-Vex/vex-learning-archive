c2b6bef1fc2ac1f79d43d492f21c2e1522c64fa5
elia <raiedaleve@gmail.com>
2026-06-19 18:59:24 +0300

    feat: add right triangle and inverted triangle pattern programs

---
+++ Programming-Archive/C/Basics/PatternDrawing/right_triangle_pattern.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("===========================================\n");
+    printf("   Welcome to Right Triangle Pattern Drawer\n");
+    printf("===========================================\n");
+    printf("Enter the size you want (1-50): ");
+
+    int size = 0;
+
+    if (scanf("%d", &size) == 1 && size > 0 && size <= 50)
+    {
+        printf("\n");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                putchar('*');
+            }
+            putchar('\n');
+        }
+        printf("\nDone! Triangle of size %d printed successfully.\n", size);
+    }
+    else
+    {
+        printf("\nInput ERROR! Please enter a number between 1 and 50.\n");
+        return 1;
+    }
+
+    return 0;
+}
