7bdbb7b3fef4542c111179e06a31ae3e178bcc94
elia <raiedaleve@gmail.com>
2026-07-06 21:31:15 +0300

    feat: add virtual_struct byte-swap test
    TODO: fix count argument passed to swap_fields (currently uses
    offset instead of 1)

--- Programming-Archive/C/MemoryManagement/byte_swap/main.c
+++ Programming-Archive/C/MemoryManagement/byte_swap/main.c
@@ -7,5 +7,18 @@
 int main()
 {
 
+    virtual_struct_t virtual_struct;
+    field_descriptor_t fields;
+
+    fields.offset = offsetof(virtual_struct_t, v1);
+    fields.size = sizeof(virtual_struct.v1);
+
+    FILE *fp = fopen("raw_bytes.bin", "rb");
+    fread(&virtual_struct, sizeof(virtual_struct_t), 1, fp);
+
+    swap_fields(&virtual_struct, &fields, fields.offset);
+
+    printf("virtual_struct : 0%lX\n", virtual_struct.v1);
+
     return 0;
 }
