d8cd04bde6ca993ce5e2145749fed678f2775835
elia <raiedaleve@gmail.com>
2026-07-22 19:03:20 +0300

    feat(cli): implement full argument parsing in main
    - Add is_int() helper to validate numeric inputs
    - Extract values for -s, -t, and -b into timer_config_t
    - Add strict error handling for invalid flags and argument counts

--- Programming-Archive/MiniProjects/vex-cli-study-timer/src/main.c
+++ Programming-Archive/MiniProjects/vex-cli-study-timer/src/main.c
@@ -1,43 +1,82 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "timer/timer.h"
+
+int is_int(char *str)
+{
+    if (*str == '+' && *(str + 1) != '\0')
+        str++;
+
+    while (*str != '\0')
+    {
+        if (!isdigit(*str) || *str == '\0')
+            return 0;
+        str++;
+    }
+    return 1;
+}
 
 int main(int argc, char *argv[])
 {
 
-    int is_session = 0;
-    int is_session_timer = 0;
-    int is_break_timer = 0;
+    if (argc != 7)
+    {
+        fprintf(stderr, "E: The vex-timer command requires exactly 6 arguments.\n");
+        fprintf(stderr, "Usage: vex-timer -s <session> -t <session_time> -b <break-time>\n");
+        return 0;
+    }
+
+    timer_config_t timer_config = {0};
 
     for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "-s") == 0)
         {
-            is_session = 1;
+            if (i + 1 < argc && is_int(argv[i + 1]) == 1)
+            {
+                timer_config.session = atoi(argv[i + 1]);
+                i++;
+            }
+            else
+            {
+                printf("E: The session value is not valid\n");
+                return 0;
+            }
         }
         else if (strcmp(argv[i], "-t") == 0)
         {
-            is_session_timer = 1;
+            if (i + 1 < argc && is_int(argv[i + 1]) == 1)
+            {
+                timer_config.session_time = atoi(argv[i + 1]);
+                i++;
+            }
+            else
+            {
+                printf("E: The session time value is not valid\n");
+                return 0;
+            }
         }
         else if (strcmp(argv[i], "-b") == 0)
         {
-            is_break_timer = 1;
+            if (i + 1 < argc && is_int(argv[i + 1]) == 1)
+            {
+                timer_config.break_time = atoi(argv[i + 1]);
+                i++;
+            }
+            else
+            {
+                printf("E: The break time value is not valid\n");
+                return 0;
+            }
         }
-    }
-
-    if (argc <= 1)
-    {
-        printf("E: The vex-timer command takes no arguments\n");
-        printf("vex-timer -s <session> -t <session_time> -b <break-time>\n");
-    }
-    else if (argc > 7)
-    {
-        printf("E: The vex-timer command takes more than 7 arguments\n");
-        printf("vex-timer -s <session> -t <session_time> -b <break-time>\n");
-    }
-    else if (argc < 7)
-    {
-        printf("E: The vex-timer command takes less than 7 arguments\n");
-        printf("vex-timer -s <session> -t <session_time> -b <break-time>\n");
+        else
+        {
+            printf("E: flag not valid\n");
+            printf("NOTE: vex-timer -s <session> -t <session_time> -b <break-time>\n");
+            return 0;
+        }
     }
 
     return 0;
