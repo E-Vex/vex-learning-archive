e6f967cd5274008d7419f4d618d621aa7970ac27
elia <raiedaleve@gmail.com>
2026-07-22 16:54:25 +0300

    feat(cli): add basic command-line argument checking in main

--- Programming-Archive/MiniProjects/vex-cli-study-timer/src/main.c
+++ Programming-Archive/MiniProjects/vex-cli-study-timer/src/main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+
+    int is_session = 0;
+    int is_session_timer = 0;
+    int is_break_timer = 0;
+
+    for (int i = 1; i < argv; i++)
+    {
+        if (strcmp(*argv, "-s") == 0)
+        {
+            is_session = 1;
+        }
+        else if (strcmp(*argv, "-t"))
+        {
+            is_session_timer = 1;
+        }
+        else if (strcmp(*argv, "-s" == 0))
+        {
+            is_break_timer = 1;
+        }
+    }
+
+    if (argc <= 1)
+    {
+        printf("E: The vex-timer command takes no arguments\n");
+        printf("vex-timer -s <session> -t <session_time> -b <break-time>\n");
+    }
+    else if (argc > 7)
+    {
+        printf("E: The vex-timer command takes more than 7 arguments\n");
+        printf("vex-timer -s <session> -t <session_time> -b <break-time>\n");
+    }
+    else if (argc < 7)
+    {
+        printf("E: The vex-timer command takes less than 7 arguments\n");
+        printf("vex-timer -s <session> -t <session_time> -b <break-time>\n");
+    }
 
     return 0;
 }
